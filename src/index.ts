26508a81cd11e5882405b84188503ffa8ed80e41
keindev <kein@tagproject.ru>
2022-04-13 11:40:37 +0300

    build: fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as effect } from './core/effect';
+export { default as switcher } from './core/switcher';
 export { default as types } from './core/types';
 export * from './constants';
