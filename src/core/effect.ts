a222c930af60654fe4194775852d4497a78215d0
keindev <kein@tagproject.ru>
2022-04-14 12:55:58 +0300

    types: fix effect generator return type

--- src/core/effect.ts
+++ src/core/effect.ts
@@ -6,7 +6,7 @@ import { IFlags } from '../types';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const effect = <P extends ModelProperties, R, Args extends any[]>(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  generator: (...args: Args) => Generator<PromiseLike<any>, R>,
+  generator: (...args: Args) => Generator<PromiseLike<any>, R, any>,
   flags: IFlags<P>
 ): readonly [(...args: Args) => Promise<FlowReturn<R>>, IFlags<P>] => [flow<R, Args>(generator), flags] as const;
 
